a687a18c2cf9032ac702fd953fe2dd812511c0b6
shivapandey63 <95334019+shivapandey63@users.noreply.github.com>
2023-03-16 20:32:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// getnumber
+function number(num) {
+    var result = document.getElementById("input");
+    result.value += num
+}
+//getresult
+function result() {
+    var result = document.getElementById("input");
+    result.value = eval(result.value)
+}
+//clear result
+function clearResult() {
+    var result = document.getElementById("input");
+    result.value = "";
+}
+
+function deleteChar() {
+    var number = document.getElementById("input");
+    var remove = number.value;
+    remove = remove.slice(0, -1); //remove last character from a strings
+    number.value = remove;
+}
